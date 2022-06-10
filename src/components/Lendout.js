247d700cc64161c56e752d1b35bbc4ffb2ab8000
gokulpr-47 <gokulpr47@gmail.com>
2022-06-10 23:53:30 +0530

    sdf

--- src/components/Lendout.js
+++ src/components/Lendout.js
@@ -1,45 +1,51 @@
-// import React from 'react'
+import React from 'react'
+import {Container, Button} from "react-bootstrap";
+import axios from "axios"
 
-// export default function Lendout(){
-//     const [bookName, setBookName] = React.useState();
-//     const [isbn, setIsbn] = React.useState();
-//     const [genre, setGenre] = React.useState();
+export default function Lendout(){
+    const [bookName, setBookName] = React.useState();
+    const [isbn, setIsbn] = React.useState();
+    const [genre, setGenre] = React.useState();
 
-//     return(
-//         <div className="lendout-form">
-//             <div className="lendout-container">
-//                 <Container>
-//                     <div className="signIn_page">
-//                         <h3>Lend out a book</h3>
-//                         {redirectText ? (
-//                         <p style={{ marginBottom: "0px", fontWeight: "500" }}>
-//                             {redirectText}
-//                         </p>
-//                         ) : (
-//                         ""
-//                         )}
-//                         <div>
-//                         <label htmlFor="username">Book's name: </label>
-//                         <input
-//                             type="text"
-//                             id="bookname"
-//                             onChange={(e) => setBookName(e.target.value)}
-//                         />
-//                         </div>
-//                         <div>
-//                         <label htmlFor="number">ISBN</label>
-//                         <input
-//                             type="number"
-//                             id="password"
-//                             onChange={(e) => setIsbn(e.target.value)}
-//                         />
-//                         </div>
-//                         <Button variant="outline-success" onClick={sendData}>
-//                         Sign In
-//                         </Button>
-//                     </div>
-//                 </Container>
-//             </div>
-//         </div>
-//     )
-// }
+    const sendData = () =>{
+        console.log("sending data")
+    }
+
+    return(
+        <div className="lendout-form">
+            <div className="lendout-container">
+                <Container>
+                    <div className="signIn_page">
+                        <h3>Lend out a book</h3>
+                        {/* {redirectText ? (
+                        <p style={{ marginBottom: "0px", fontWeight: "500" }}>
+                            {redirectText}
+                        </p>
+                        ) : (
+                        ""
+                        )} */}
+                        <div>
+                        <label htmlFor="username">Book's name: </label>
+                        <input
+                            type="text"
+                            id="bookname"
+                            onChange={(e) => setBookName(e.target.value)}
+                        />
+                        </div>
+                        <div>
+                        <label htmlFor="number">ISBN</label>
+                        <input
+                            type="number"
+                            id="password"
+                            onChange={(e) => setIsbn(e.target.value)}
+                        />
+                        </div>
+                        <Button variant="outline-success" onClick={sendData}>
+                        Sign In
+                        </Button>
+                    </div>
+                </Container>
+            </div>
+        </div>
+    )
+}
